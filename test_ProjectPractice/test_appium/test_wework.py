667db81fefa776be88bff6dd4369b625f74bdd74
wei1.wang <wei1.wang@ximalaya.com>
2021-03-05 15:45:23 +0800

    3/4app企业微信实战

--- test_ProjectPractice/test_appium/test_wework.py
+++ test_ProjectPractice/test_appium/test_wework.py
@@ -17,7 +17,7 @@ from appium.webdriver.common.mobileby import MobileBy
 from appium.webdriver.common.touch_action import TouchAction
 
 
-class TestDW():
+class TestSign():
     def setup(self):
         des_caps = {
             "platformName": "android",
@@ -42,10 +42,12 @@ class TestDW():
             # "appPackage": "om.tencent.mobileqq",
             # "appActivity": "com.tencent.mobileqq.activity.SplashActivity",
 
-            "deviceName": "T3Q6T16301006992",  # honor：T3Q6T16301006992   #oppo:1b3129a9
+            # "deviceName": "T3Q6T16301006992",  # honor：T3Q6T16301006992   #oppo:1b3129a9
+            "deviceName": "emulator-5554",  # honor：T3Q6T16301006992   #oppo:1b3129a9
             "noReset": True,  # 去掉页面弹窗，提升云效速度
             "skipServerInstallation": 'true',  # 跳过安装、权限设置等操作(提升app运行速度),
             "skipDeviceInitialization": 'true',
+            "settings[waitForIdleTimeout]": 1, # settings 控制 动态页面的等待时长
             'automationName': 'UiAutomator1',
         }
 
@@ -56,11 +58,31 @@ class TestDW():
     def teardown(self):
         self.driver.quit()
 
-    def test_webview(self):
-        # time.sleep(5)
-        el1 = self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/igk")
-        el1.click()
-        el2 = self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/gy9")
-        el2.send_keys("王")
-        # el3 = self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/gy9")
+    def test_sign(self):
+        '''
+        前提条件：已登录
+        打卡用例：
+        1、打开【企业微信】应用
+        2、进入【工作台】
+        3、点击【打卡】
+        4、选择【外出打卡】tab
+        5、点击【第N次打卡】
+        6、验证【外出打卡成功】
+        7、退出【企业微信】应用
+        :return:
+        '''
+        self.driver.find_element(MobileBy.XPATH, "//android.view.ViewGroup//*[@text='工作台']").click()
+        # 滑动查找  目前是向下滑动两次，再向上查找，知道找到元素
+        self.driver.find_element(MobileBy.ANDROID_UIAUTOMATOR, 'new UiScrollable(new UiSelector().'
+                                                        'scrollable(true).instance(0)).'
+                                                        'scrollIntoView(new UiSelector().text("打卡").'
+                                                        'instance(0));').click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡']").click()
+        self.driver.find_element(MobileBy.XPATH, '//*[contains(@text, "次外出")]').click()
+        print(self.driver.page_source) #打印当前页面结构
+        time.sleep(2)
+        assert "外出打卡成功" in self.driver.page_source
+
+        # 激活隐式等待
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='外出打卡成功']")
 
