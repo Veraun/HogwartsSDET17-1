d5fc7e9fbd3e8d138f61df4ab1f3749c14b143ff
wei1.wang <wei1.wang@ximalaya.com>
2021-03-12 17:03:41 +0800

    3/12 课后作业

--- ui_framework/page/base_page.py
+++ ui_framework/page/base_page.py
@@ -15,16 +15,46 @@ import logging
 
 from appium.webdriver.common.mobileby import MobileBy
 from appium.webdriver.webdriver import WebDriver
+from selenium.common import exceptions
 from selenium.common.exceptions import NoSuchElementException
 
-
 class BasePage:
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename="../logs/app.log",
+                        format="[%(asctime)s-%(filename)s-%(levelname)s:%(message)s]", filemode="a",
+                        datefmt="'%Y-%m-%d %I:%M:%S %p'")
 
     def __init__(self, driver: WebDriver = None):
         self.driver = driver
 
-    def find(self, locator, value):
+    def black(self, func):
+        black_list = ['//*[@resource-id="com.xueqiu.android:id/iv_close"]']
+
+        def wrapper(*args, **kwargs):
+            try:
+                print(f"进入装饰器,{func.__name__}")
+                obj = func(*args, **kwargs)
+                if len(obj) == 0:
+                    raise NoSuchElementException("未找到此元素")
+                return obj
+            except exceptions :
+                for ele_xpath in black_list:
+                    # 用火眼金睛去看，妖魔鬼怪是否存在
+                    eles = self.finds(MobileBy.XPATH, ele_xpath)
+                    # 妖魔鬼怪出现了，需要斩杀
+                    if len(eles) > 0:
+                        eles[0].click()
+                    return func(*args, **kwargs)
+                print("出来装饰器")
+
+        return wrapper
+
+    @black
+    def find_new(self, locator, value):
+        logging.info("find")
+        logging.info("定位方法是:%s,定位元素是：%s", locator, value)
+        return self.driver.find_element(locator, value)
+
+    def find_old(self, locator, value):
         black_list = ['//*[@resource-id="com.xueqiu.android:id/iv_close"]']
         try:
             logging.info("find")
@@ -38,7 +68,7 @@ class BasePage:
                 # 妖魔鬼怪出现了，需要斩杀
                 if len(eles) > 0:
                     eles[0].click()
-                    return self.find(locator, value)
+                    return self.find_old(locator, value)
 
     def finds(self, locator, value):
         logging.info("finds")
@@ -50,7 +80,7 @@ class BasePage:
         logging.info("find and click")
         logging.info("定位方法是:%s,定位元素是：%s", locator, value)
         # self.driver.find_element(locator, value).click()
-        self.find(locator, value).click()
+        self.find_new(locator, value).click()
 
     def find_and_sendKeys(self, locator, value, text):
         logging.info("定位方法是:%s,定位元素是：%s,文本输入的是：%s", locator, value, text)
