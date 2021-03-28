06626ff1c74e39fb21c5038ee95810f389932fad
wei1.wang <wei1.wang@ximalaya.com>
2021-03-28 22:10:52 +0800

    自动化-企业微信接口自动化作业【二】

---
+++ test_ProjectPractice/test_requests/testcase/TestAddress.py
@@ -0,0 +1,9 @@
+'''
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+@author: wangwei
+@project: HogwartsSDET17
+@file: TestAddress.py
+@time: 2021/3/28 19:33
+@Email: Warron.Wang
+'''
---
+++ test_ProjectPractice/test_requests/wework/WeWorkAddress.py
@@ -0,0 +1,9 @@
+'''
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+@author: wangwei
+@project: HogwartsSDET17
+@file: WeWorkAddress.py
+@time: 2021/3/28 19:31
+@Email: Warron.Wang
+'''
